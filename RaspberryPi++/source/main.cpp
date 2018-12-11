dfb7c938b91a1c2019b1fafa27698d794b4731bf
MatthijsHeidema <17117100@student.hhs.nl>
2018-12-11 16:04:33 +0100

    Made the main class for syncing the lamp

--- RaspberryPi++/source/main.cpp
+++ RaspberryPi++/source/main.cpp
@@ -2,18 +2,23 @@
 // Geeks for Geeks and additions by Mathieu
 
 #define FILE_PATH "/home/pi/Documents/PI_client/client.json"
+#define FILE_PATH_LAMP "/var/www/html/lamp.json"
+#define IP_LAMP "192.168.3.1"
+#define PORT_LAMP 3000
 
 #include <stdio.h>
-#include "JsonFile.h"
-#include "TCP_Client.h"
-#define PORT 3000
+#include "Lamp.h"
 
 using namespace std;
 
 int main()
 {
-	TCP_Client a("192.168.178.5", PORT);
-	a.connectToServer();
+	Lamp lamp(IP_LAMP, FILE_PATH_LAMP, PORT_LAMP);
+
+	while(1) {
+
+		lamp.sync();
+	}
 
 	return 0;
 }
