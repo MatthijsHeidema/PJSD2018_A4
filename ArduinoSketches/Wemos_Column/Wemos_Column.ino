620e9d929b4084faa11a81bcdd55bf4c4c8958f4
Jerry Jonis <jersjo@hotmail.com>
2018-12-18 14:42:22 +0100

    Column code merged with server code

--- ArduinoSketches/Wemos_Column/Wemos_Column.ino
+++ ArduinoSketches/Wemos_Column/Wemos_Column.ino
@@ -10,44 +10,37 @@ char buffer_array[3];
 int i = 0, analog[2], movement_counter;
 bool new_data_flag = false, led_status = false, led_update = false, in_bed = false, 
      epilepsy_attack = false, bed_update = false, epilepsy_timer_start = true, rookMelder, Rookstatus;
-String request;
 unsigned char schakelaar;
 unsigned long time_led_on, time_led_current, time_in_bed, time_bed_current;
 
+String request;
+const char* preceived = "";
+char key[64];
+char value[64];
+bool state;
+
 WiFiServer wifiServer(3000);
 
-void wemosFi()
+const char* ssid = "PIWORKS";
+const char* password = "PInetPASSword123";
+  
+// Static ip address
+IPAddress ip(192, 168, 4, 4);         // where xx is the desired IP Address
+IPAddress gateway (192, 168, 4, 1);   // set gateway to match your network 
+IPAddress subnet(255, 255, 255, 0);   // set subnet mask to match your network
+  
+void wifiSetup()
 {
-
-  const char* ssid = "PIWORKS";
-  const char* password = "PInetPASSword123";
-  
-  // Static ip address
-  IPAddress ip(192, 168, 4, 4);         // where xx is the desired IP Address
-  IPAddress gateway (192, 168, 4, 1);   // set gateway to match your network 
-  IPAddress subnet(255, 255, 255, 0);   // set subnet mask to match your network
-  
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
   WiFi.config(ip, gateway, subnet);
-
-  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
-     would try to act as both a client and an access-point and could cause
-     network-issues with your other WiFi-devices on your WiFi-network. */
-  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  // While wifi is not connected 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(1000);
+    Serial.println("Connecting..");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+
+  Serial.print("Connected to WiFi. IP:");
   Serial.println(WiFi.localIP());
+
   wifiServer.begin();
 }
 
@@ -64,32 +57,32 @@ void setDDR(int IO){
   Wire.endTransmission();
 }
 
-void setOutput(int output_address, bool output_status)
+void setOutput(int output_address, bool output_status) // Function for setting the output registers on the WIB. Requires the specific bit and true or false
 {
   if(output_status == AAN){    
-    Wire.beginTransmission(0x38); 
-    Wire.write(byte(0x01));            
-    Wire.write(byte(output_address << 4));            
-    Wire.endTransmission();
+    Wire.beginTransmission(0x38);           // Send a write byte to the I2C device
+    Wire.write(byte(0x01));                 // Telling the I2C device what byte we want to write to
+    Wire.write(byte(output_address << 4));  // Setting the bits           
+    Wire.endTransmission();                 // Ending the transmission. An I2C stop byte has been sent
   }
   if(output_status == UIT){
-    Wire.beginTransmission(0x38); 
-    Wire.write(byte(0x01));            
-    Wire.write(byte(0 << 4));            
-    Wire.endTransmission();
+    Wire.beginTransmission(0x38);           // Send a write byte to the I2C device
+    Wire.write(byte(0x01));                 // Telling the I2C device what byte we want to write to
+    Wire.write(byte(0 << 4));               // Setting the bits
+    Wire.endTransmission();                 // Ending the transmission. An I2C stop byte has been sent
   }
 }
 
-void readAnalog(int* analog_values)
-{
-  Wire.requestFrom(0x36, 4);   
-  unsigned int anin0 = Wire.read()&0x03;  
-  anin0=anin0<<8;
-  anin0 = anin0|Wire.read();  
+void readAnalog(int* analog_values)          // Function for reading an 10 bit analog value from the WIB. Requires an pointer to an analog values array
+{         
+  Wire.requestFrom(0x36, 4);                 // Send a read byte to the I2C device
+  unsigned int anin0 = Wire.read()&0x03;     // Reading the 2 Most Significant Bits from analog 0 input on the WIB
+  anin0=anin0<<8;                            // Shifting these 2 bits 8 places to give these bits their original value
+  anin0 = anin0|Wire.read();                 // Store the last 8 bits to their original value. (OR comparision 1 - 0 == 1)
   
-  unsigned int anin1 = Wire.read()&0x03;  
-  anin1=anin1<<8;
-  anin1 = anin1|Wire.read();
+  unsigned int anin1 = Wire.read()&0x03;     // Reading the 2 Most Significant Bits from analog 1 input on the WIB
+  anin1=anin1<<8;                            // Shifting these 2 bits 8 places to give these bits their original value
+  anin1 = anin1|Wire.read();                 //Store the last 8 bits to their original value. (OR comparision 1 - 0 == 1
   
   Serial.print("analog in 0: ");
   Serial.println(anin0);    
@@ -97,12 +90,8 @@ void readAnalog(int* analog_values)
   /*Serial.print("analog in 1: ");
   Serial.println(anin1);   
   Serial.println("");*/
- 
   analog_values[0] = anin0;
   analog_values[1] = anin1;
-
-    
-  
 }
 
 int readSwitch(int switch_bit)
@@ -147,6 +136,44 @@ void button(int schakelaar)
   }
 }
 
+int splitInto(const char* received, char key[64], char value[64]) {
+
+  const char *tracker = received;     //pointer to go through received
+  int command = 0;                    //differentiate between key and value
+
+  //Empty key and value arrays
+  memset(&key[0], 0, 64);
+  memset(&value[0], 0, 64);
+
+  for (int i = 0; i < 63 && *tracker; i++, tracker++) { //loops untill \0 is found or max size of arrays is reached
+
+    if (*tracker == ':') {
+      command = 1;                                     //received is a command, the rest of the input is the value associated with it
+      i = -1;                                          //reset i (will become 0 before next iteration)
+      continue;
+    }
+    if (command) {                                     //if ':' was found, write the rest of the string to value
+      value[i] = *tracker;
+
+    } else if (*tracker == '?') {                     //if '?' is found, received is a request, no value is given
+      value[0] = '?';                                 //write "?" to value
+      return 0;                                       //return
+    } else {                                          //if neither ':' nor '?' was found, continue writing to key
+      key[i] = *tracker;
+    }
+  }
+  if (command) return 0;                              //if neither ':' nor '?' was found before end of received, it is invalid
+  return 1;
+}
+
+void Status(char value[]){
+  if (!strcmp(value, "0")) {
+    state = false;
+  } else if (!strcmp(value, "1")) {
+    state = true;
+  }
+}
+
 void smokeAlarm()
 {
   readAnalog(analog);
@@ -170,37 +197,61 @@ void smokeAlarm()
     }
 }
 
-void setup() 
-{
-  Wire.begin();
-  Serial.begin(115200);
-  delay(1000);
-  setDDR(0x01);
- // wemosFi(); 
+void setup() {
+  Wire.begin();                         // Starting the I2C communication
+  Serial.begin(115200);                 // Starting the I2C communication
+  wifiSetup();                          // Starting the WiFi server
 }
 
-void loop() 
-{
+void loop() {
   WiFiClient client = wifiServer.available();
 
-  if (client) {
-    while (client.connected()) {
-      while (client.available() > 0) {
+  if (client)
+  {
+    while (client.connected())
+    {
+      while (client.available() > 0)
+      {
         request = client.readStringUntil('\r');
-        Serial.println(request);        
-      }
-      //CODE HERE!!!
+        preceived = request.c_str();
+        Serial.println(preceived);
+        Serial.print("De pi zond: ");
+        Serial.println(request);
+        //CODE HERE!!!
         smokeAlarm();
-        client.write(analog[0]);  // analog value
-        client.write(Rookstatus); // Alarm status 
-        client.write(rookMelder); // Buzzer status
-      delay(250); 
+        client.write(analog[0]);  // send to analog value to server
+        client.write(Rookstatus); // send Alarm status to server
+        client.write(rookMelder); // send Buzzer status to server
+      }
+
+      if (!splitInto(preceived, key, value))
+      {
+        Serial.print("key: ");
+        Serial.print(key);
+        Serial.print("; value: ");
+        Serial.println(value);
+
+        if (!strcmp(key, "ToggleLed"))
+        {
+          //Status(value);
+
+        } else if (!strcmp(key, "Color"))
+        {
+          //colorPicker(value);
+
+        } else if (!strcmp(key, "PIR"))
+        {
+          //Send PIR value
+          //String pir_val = String(inputs & 0x0F);
+          //client.write(pir_val.c_str(), 64);
+        } else
+        {
+          Serial.println("Unknown command");
+        }
+      } else
+      {
+        Serial.println("Invalid input");
+      }
     }
-    client.stop();
-    Serial.println("Client disconnected");
-  }else{
-    
-    
   }
-   
 }
