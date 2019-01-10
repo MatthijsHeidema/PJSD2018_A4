3217d6af6c69c1feba90ba1d5434f58074fbdf22
ElmerHHS <16076567@student.hhs.nl>
2019-01-10 17:16:15 +0100

    added forms to customise time until notification

--- WebServer/html/Bewaker.php
+++ WebServer/html/Bewaker.php
@@ -6,14 +6,14 @@
 		<link rel="stylesheet" type="text/css" href="style.css"/>
 	</head>
 	<meta http-equiv="refresh" content="2">
-	
+
 	<body>
 		<ul>
 			<li><a href="index.php">Timothy</a></li>
 			<li><a class="active" href="Bewaker.php">Guard</a></li>
 			<li><a href="Verpleegster.php">Nurse</a></li>
 		</ul>
-			
+
 		<?php
 			ini_set('display_errors', 'On');
 			error_reporting(E_ALL);
@@ -24,7 +24,7 @@
 			define("FRIDGE_PATH", "fridge.json");
 			define("DOOR_PATH", "door.json");
 			define("MAIN_PATH", "main.json");
-			
+
 			function updatePHP($path) {
 				$jsonFile = fopen($path, "r") or die("Unable to open file");
 				$jsonString = fread($jsonFile, filesize($path));
@@ -46,50 +46,53 @@
 			$fridge = updatePHP(FRIDGE_PATH);
 			$door = updatePHP(DOOR_PATH);
 			$main = updatePHP(MAIN_PATH);
-			
-			if ($door->{'ToggleDoor'}) {
+
+			if ($door->{'doorStatus'}) {
 				$doorStatus = "open";
 			} else {
 				$doorStatus = "gesloten";
 			}
+
+			$updateTimeDay = $bed->{'updateTimeDay'};
+			$updateTimeNight = $bed->{'updateTimeNight'}; 
 		?>
-		
+
 		<h1> Home Automation System for Security Guard </h1></br>
-		
+
 		<table style="width:100%">
 			<td>
 				<h2> Alarms:</h2><br>
-				
+
 				<?php if ($bed->{'OutOfBed'}) { ?>
 				Client is te lang uit bed! <form method="post">
 					<input type="submit" name="Ack_OutOfBed" value="Stop Alarm" />
 				</form><br>
 				<?php } ?>
-				
+
 				<?php if ($bed->{'EpilepsieAanvalBed'}) { ?>
 				Epileptische aanval in bed! <form method="post">
 					<input type="submit" name="Ack_EpilepsyBed" value="Stop Alarm" />
 				</form><br>
 				<?php } ?>
-				
+
 				<?php if ($chair->{'EpilepsieAanvalStoel'}) { ?>
 				Epileptische aanval op de stoel! <form method="post">
 					<input type="submit" name="Ack_EpilepsyChair" value="Stop Alarm" />
 				</form><br>
 				<?php } ?>
-				
+
 				<?php if ($pillar->{'SmokeAlarm'}) { ?>
 				Rook gedetecteerd! <form method="post">
 					<input type="submit" name="Ack_Smoke" value="Stop Alarm" />
 				</form><br>
 				<?php } ?>
-				
+
 				<?php if ($fridge->{'OpenTooLong'}) { ?>
 				Koelkast te lang open! <form method="post">
 					<input type="submit" name="Ack_Fridge" value="Stop Alarm" />
 				</form><br>
 				<?php } ?>
-				
+
 				<?php if ($main->{'TooLongAbsent'}) { ?>
 				Client te lang niet gedetecteerd! <form method="post">
 					<input type="submit" name="Ack_Missing" value="Stop Alarm" />
@@ -98,50 +101,74 @@
 			</td>
 			<td>
 				<form method="post">
-					<input type="submit" name="ToggleDoor" value="Deur: <?php echo $doorStatus; ?>" />
-				</form><br>
+					<input type="submit" name="doorStatus" value="Deur: <?php echo $doorStatus; ?>" />
+				</form><br><br><br><br>
+				<form method="post">
+					<input type="submit" value="Tijd voor melding overdag: <?php echo $updateTimeDay; ?>" />
+					<input type="number" style="width: 7em" name="updateTimeDay" id="updateTimeDay" min="0" max="3600" />
+				</form>
+				<form method="post">
+					<input type="submit" value="Tijd voor melding 's nachts: <?php echo $updateTimeNight; ?>" />
+					<input type="number" style="width: 7em" name="updateTimeNight" id="updateTimeNight" min="0" max="3600" /><br>
+				</form>
 			</td>
 
-		
+
 		<?php
 				if(array_key_exists('Ack_OutOfBed', $_POST)){
 					$bed->{'OutOfBed'} = "0";
 					updateJson($bed, BED_PATH);
 				}
-				
+
 				if(array_key_exists('Ack_Missing', $_POST)){
 					$main->{'TooLongAbsent'} = "0";
 					updateJson($main, MAIN_PATH);
 				}
-				
+
 				if(array_key_exists('Ack_EpilepsyBed', $_POST)){
-					$bed->{'EpilepsieAanval'} = "0";
+					$bed->{'EpilepsieAanvalBed'} = "0";
 					updateJson($bed, BED_PATH);
 				}
-				
+
 				if(array_key_exists('Ack_EpilepsyChair', $_POST)){
-					$chair->{'EpilepsieAanval'} = "0";
+					$chair->{'EpilepsieAanvalStoel'} = "0";
 					updateJson($chair, CHAIR_PATH);
 				}
-				
+
 				if(array_key_exists('Ack_Smoke', $_POST)){
 					$pillar->{'SmokeAlarm'} = "0";
 					updateJson($pillar, PILLAR_PATH);
 				}
-				
+
 				if(array_key_exists('Ack_Fridge', $_POST)){
 					$fridge->{'OpenTooLong'} = "0";
 					updateJson($fridge, FRIDGE_PATH);
 				}
-				
-				if(array_key_exists('ToggleDoor', $_POST)){
-					
-					if ($door->{'ToggleDoor'}) {
-						$door->{'ToggleDoor'} = "0";
+
+				if(array_key_exists('doorStatus', $_POST)){
+
+					if ($door->{'doorStatus'}) {
+						$door->{'doorStatus'} = "0";
 					} else {
-						$door->{'ToggleDoor'} = "1";
+						$door->{'doorStatus'} = "1";
 					}
 					updateJson($door, DOOR_PATH);
+				}
+
+				if(array_key_exists('updateTimeDay', $_POST))
+				{
+					$bed->{'updateTimeDay'} = (int)$_POST["updateTimeDay"];
+					$chair->{'updateTimeDay'} = (int)$_POST["updateTimeDay"];
+					updateJson($bed, BED_PATH);
+					updateJson($chair, CHAIR_PATH);
+				}
+
+				if(array_key_exists('updateTimeNight', $_POST))
+				{
+					$bed->{'updateTimeNight'} = (int)$_POST["updateTimeNight"];
+					$chair->{'updateTimeNight'} = (int)$_POST["updateTimeNight"];
+					updateJson($bed, BED_PATH);
+					updateJson($chair, CHAIR_PATH);
 				}
 		?>
 </body>
